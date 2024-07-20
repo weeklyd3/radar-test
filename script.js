011d8df86876a91973560148778c8a03fcc8079c
weeklyd3 <timothywei.spam@gmail.com>
2024-07-20 03:33:19 +0000

    textures are messed up but the code is there

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function drawMap(draw, width, height) {
+	display2_header.clear();
+	display2_header.fill('black');
+	display2_header.rect(0, 0, width, height);
+	display2_header.erase();
+	display2_header.circle(width / 2, height * 0.95, height * 1.7);
+	display2_header.noErase();
 	draw.clear();
 	draw.push();
 	draw.background('black');
@@ -9,8 +15,14 @@ function drawMap(draw, width, height) {
 	var map_radius = height * 0.85;
 	draw.translate(...map_center);
 	draw.rotate(-map_heading);
-	draw.push();
+	draw.fill(draw.color(0, 0, 0, 0));
 	draw.stroke('white');
+	draw.rotate(player.heading);
+	drawWaypoints(draw, width, height);
+	draw.image(display2_header, -width / 2, -height * 0.95, width, height);
+	draw.rotate(-player.heading);
+	draw.stroke('white');
+	draw.fill('white');
 	draw.strokeWeight(0);
 	for (var i = 0; i < 36; i++) {
 		draw.text(i, 0, -map_radius - height * 0.01);
@@ -23,12 +35,9 @@ function drawMap(draw, width, height) {
 		draw.line(0, -map_radius, 0, -map_radius + height * 0.03);
 		draw.rotate(5);
 	}
-	draw.pop();
 	draw.fill(draw.color(0, 0, 0, 0));
 	draw.stroke('white');
 	draw.circle(0, 0, map_radius * 2);
-	draw.rotate(player.heading);
-	drawWaypoints(draw, width, height);
 	draw.pop();
 }
 function loadZone(name) {
@@ -55,11 +64,22 @@ function drawWaypoints(draw, width, height) {
 	draw.strokeWeight(0);
 	var position = [player.x, player.y];
 	var range = player.map_range;
-	var offset = Math.ceil(range / 400);
+	var offset = Math.ceil(range / 500);
 	var min_range = [player.zoneX - offset, player.zoneY - offset];
 	var max_range = [player.zoneX + offset, player.zoneY + offset];
 	for (var x = min_range[0]; x <= max_range[0]; x++) {
 		for (var y = min_range[1]; y <= max_range[1]; y++) {
+			if (player.map_texture) {
+				if (!textures[player.map_texture]) textures[player.map_texture] = {};
+				var texture_object = textures[player.map_texture];
+				if (!texture_object[`${x},${y}`]) {
+					var url = `textures/${player.map_texture}/${x},${y}.png`;
+					console.log(url);
+					texture_object[`${x},${y}`] = globalDraw.loadImage(url);
+				}
+				target = texture_object[`${x},${y}`];
+				if (target) draw.image(texture_object[`${x},${y}`], ((x - player.zoneX) * 500 - player.zoneXOffset) * ratio, ((y - player.zoneY) * 500 - player.zoneYOffset) * ratio, 500 * ratio, 500 * ratio);
+			}
 			if (!waypoints[x + ',' + y]) {
 				console.log('LOAD', x + ',' + y)
 				loadZone(x + ',' + y);
@@ -75,15 +95,17 @@ function drawWaypoints(draw, width, height) {
 					dx *= ratio;
 					dy *= ratio;
 					draw.circle(dx, dy, 3);
-					draw.text(wpt.name.toUpperCase(), dx, dy + 10);
+					draw.text(wpt.name.toUpperCase(), dx, dy - 10);
 				}
 			}
 		}
 	}
 }
 function update() {
-	player.zoneX = Math.floor(player.x / 400);
-	player.zoneY = Math.floor(player.y / 400);
+	player.zoneX = Math.floor(player.x / 500);
+	player.zoneY = Math.floor(player.y / 500);
+	player.zoneXOffset = player.x - 500 * player.zoneX;
+	player.zoneYOffset = player.y - 500 * player.zoneY;
 	draw.clear();
 	drawMap(display2, width / 2, height);
 	draw.image(display1, 0, 0);
@@ -111,6 +133,8 @@ var s = function (sketch) {
 		display2.angleMode('degrees');
 		display2.background('black');
 		display2.textAlign('center', 'center');
+		display2_header = draw.createGraphics(width / 2, height);
 	}
 };
 var draw = new p5(s, 'pad');
+var globalDraw = draw;
