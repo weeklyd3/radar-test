bf05ba6793850a734a05484ca090ff9a2a258f55
weeklyd3 <timothywei.spam@gmail.com>
2024-07-22 00:32:03 +0000

    mess things up

--- displays.js
+++ displays.js
@@ -8,6 +8,7 @@ function drawMap(draw, width, height) {
 		if (currentRatio > 10) currentRatio = 10;
 		player.map_range = height * 0.85 * currentRatio;
 	}
+	if (player.map_range > (height * 8.5)) player.map_range = height * 8.5;
 	display2_header.clear();
 	display2_header.fill('black');
 	display2_header.rect(0, 0, width, height);
@@ -28,6 +29,7 @@ function drawMap(draw, width, height) {
 	drawWaypoints(draw, width, height);
 	draw.fill(draw.color(0, 0, 0, 0));
 	draw.strokeWeight(1);
+	draw.stroke('white');
 	draw.triangle(-10, 14, 10, 14, 0, -14);
 	draw.strokeWeight(0);
 	draw.image(display2_header, -width / 2, -height * 0.95, width, height);
@@ -51,12 +53,34 @@ function drawMap(draw, width, height) {
 	draw.circle(0, 0, map_radius * 2);
 	draw.push();
 	draw.rotate(player.heading);
-	draw.fill('white');
+	if (player.route.length) {
+		draw.fill('yellow');
+		draw.rotate(player.route[0].heading - player.heading);
+		draw.rect(-width / 26, -height * 0.85 - 12, width / 13, 12);
+		draw.strokeWeight(0);
+		draw.fill('black');
+		draw.text(Math.round(player.route[0].heading), 0, -height * 0.85 - 6);
+		draw.rotate(-player.route[0].heading + player.heading);
+	}
+	draw.strokeWeight(1);
+	draw.fill('magenta');
+	draw.rect(-width / 26, -height * 0.85 - 12, width / 13, 12);
+	draw.stroke('magenta');
+	draw.line(0, -14, 0, -map_radius);
+	draw.stroke('white');
+	draw.fill('black');
 	draw.strokeWeight(0);
+	draw.text(Math.round(player.heading), 0, -height * 0.85 - 6);
+	draw.fill('white');
 	draw.textAlign('right', 'center');
-	draw.text('map zoom ×' + Math.round(10 * player.map_range / map_radius) / 10 + "\nrange " + Math.round(player.map_range) + " px", width / 2, -height * 0.85);
+	var waypoint_info = '';
+	if (player.route.length > 0) {
+		const wpt = player.route[0];
+		waypoint_info = `${wpt.name} ${Math.round(wpt.heading)}deg ${(wpt.distance / 1000).toFixed(2)}mi\n`
+	}
+	draw.text(waypoint_info + 'map zoom ×' + Math.round(10 * player.map_range / map_radius) / 10 + "\nrange " + Math.round(player.map_range) + " px", width / 2, -height * 0.83);
 	draw.textAlign('left', 'center');
-	draw.text(`${Math.round((player.velocity[0] ** 2 + player.velocity[1] ** 2) ** 0.5 * 24 / 1000 * 3600)} mph`, -width / 2, -height * 0.85);
+	draw.text(`${player.autopilot ? "AUTOPILOT " : "MANUAL RUD "}| ${player.engine.autothrottle ? "CRUISE CTRL" : "MANUAL THR"}\n${player.engine.parking_brake ? "PARKING BRAKE!!! [b]\n" : ""}${Math.round((player.velocity[0] ** 2 + player.velocity[1] ** 2) ** 0.5 * 24 / 1000 * 3600)} mph\nthr ${Math.round(3500 * player.engine.current_n1)}\ndrag ${Math.round(drag((player.velocity[0] ** 2 + player.velocity[1] ** 2) ** 0.5) * 24 * 24)}`, -width / 2, -height * 0.83);
 	draw.rect(-width / 6, -height * 0.93, width / 3, 2);
 	draw.rect(-width / 6, -height * 0.93, 2, 10);
 	draw.rect(width / 6 - 2, -height * 0.93, 2, 10);
@@ -156,6 +180,153 @@ function drawEngine(draw, width, height) {
 	}
 	draw.pop();
 }
+function drawMenu(draw, width, height) {
+	draw.clear();
+	draw.background('black');
+	draw.push();
+	draw.strokeWeight(0);
+	var display_route = [...player.route];
+	var you = {
+		x: player.x,
+		y: player.y,
+		name: "<current position>",
+		distance: 0,
+		heading: player.heading,
+	}
+	display_route.splice(0, 0, you);
+	var start_index = 0;
+	draw.textSize(18);
+	draw.fill('blue');
+	draw.rect(0, 0, width, 30);
+	draw.fill('white');
+	draw.text('ROUTE', width / 2, 15);
+	var entries = Math.floor(height / 30) - 3;
+	draw.translate(width / 2, 15);
+	var missed_points = 0;
+	start_index = Math.floor(selected_index / entries) * entries;
+	for (var i = start_index; i < (start_index + entries); i++) {
+		var point = display_route[i];
+		if (!point) {
+			missed_points += 1;
+			continue;
+		}
+		draw.translate(0, 30);
+		if (i == selected_index) {
+			draw.fill('green');
+			draw.rect(-width / 2, -15, width, 30);
+			draw.fill('white');
+		}
+		draw.textAlign('left', 'center');
+		draw.text(point.name, -width / 2, 0);
+		draw.text(`${point.heading.toFixed(0)} deg/${(point.distance / 1000).toFixed(2)} mi`, 0, 0);
+	}
+	draw.translate(0, 30 * missed_points + 30);
+	var msg = "up/down = select, delete = delete, a = add after";
+	if (menu_input) msg = "type to enter, enter = ok, esc = cancel";
+	if (menu_timing >= 0) {
+		menu_timing -= 1;
+		msg = menu_message;
+	}
+	draw.text(msg, -width / 2, 0);
+	if (menu_input) {
+		draw.translate(0, 30);
+		draw.text(menu_input_msg + "█", -width / 2, 0);
+	}
+	draw.pop();
+}
+addEventListener('keydown', function(ev) {
+	if (player.display != 3) return;
+	if (!menu_input) {
+		if (ev.keyCode == 86) player.display = -1;
+		if (ev.keyCode == 40) selected_index += 1;
+		if (ev.keyCode == 38) selected_index -= 1;
+		if (ev.keyCode == 8 || ev.keyCode == 46) {
+			if (selected_index == 0) {
+				menu_message = "You can't delete that!";
+				menu_timing = 48;
+			} else {
+				console.log(player.route[selected_index - 1]);
+				var name = player.route[selected_index - 1].name;
+				var index = player.route_points.indexOf(name.toLowerCase());
+				if (index != -1) {
+					player.route_points.splice(index, 1);
+				}
+				player.route.splice(selected_index - 1, 1);
+			}
+		}
+		if (ev.keyCode == 65) menu_input = true;
+		if (selected_index < 0) selected_index = player.route.length;
+		if (selected_index > player.route.length) selected_index = 0;
+	} else {
+		if (ev.keyCode == 8) menu_input_msg = menu_input_msg.slice(0, -1);
+		if (48 <= ev.keyCode && ev.keyCode < 58) menu_input_msg += '0123456789'[ev.keyCode - 48];
+		if (ev.keyCode == 27) menu_input = false;
+		if (ev.keyCode == 32) menu_input_msg += ' ';
+		if (ev.keyCode >= 65 && ev.keyCode <= 90) menu_input_msg += ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'[ev.keyCode - 65]);
+		if (ev.keyCode == 173) menu_input_msg += '-';
+		if (ev.keyCode == 13) {
+			var input = menu_input_msg.trim().toLowerCase();
+			if (all_waypoints[input]) {
+				var point = all_waypoints[input];
+				var new_point = {
+					name: input.toUpperCase(),
+					x: point.x,
+					y: point.y
+				};
+				if (selected_index > 0) {
+					var previous_point = player.route[selected_index - 1];
+					new_point.distance = ((new_point.x - previous_point.x) ** 2 + (new_point.y - previous_point.y) ** 2) ** 0.5;
+					new_point.heading = Math.atan2(new_point.x - previous_point.x, -new_point.y + previous_point.y) * 180 / Math.PI;
+				}
+				player.route.splice(selected_index, 0, new_point);
+				player.route_points.push(input);
+				menu_input = false;
+				menu_input_msg = '';
+				selected_index += 1;
+				recalculateHeadings(player.route);
+				return;
+			}
+			if (input.includes(' ')) {
+				var split = input.split(' ');
+				if (split.length == 2) {
+					var numbers = [Number(split[0]), Number(split[1])];
+					if (numbers[0] == numbers[0] && numbers[1] == numbers[1]) {
+						var new_point = {
+							name: `X${Math.round(numbers[0])}Y${Math.round(numbers[1])}`,
+							x: numbers[0],
+							y: numbers[1]
+						};
+						if (selected_index > 0) {
+							var previous_point = player.route[selected_index - 1];
+							new_point.distance = ((new_point.x - previous_point.x) ** 2 + (new_point.y - previous_point.y) ** 2) ** 0.5;
+							new_point.heading = Math.atan2(new_point.x - previous_point.x, -new_point.y + previous_point.y) * 180 / Math.PI;
+						}
+						player.route.splice(selected_index, 0, new_point);
+						menu_input = false;
+						menu_input_msg = '';
+						selected_index += 1;
+							recalculateHeadings(player.route);
+						return;
+					}
+				}
+			}
+		}
+	}
+})
+function recalculateHeadings(points) {
+	var position = [player.x, player.y];
+	for (const p of points) {
+		p.distance = ((p.x - position[0]) ** 2 + (p.y - position[1]) ** 2) ** 0.5;
+		p.heading = Math.atan2(p.x - position[0], -p.y + position[1]) * 180 / Math.PI;
+		position[0] = p.x;
+		position[1] = p.y;
+	}
+}
+var menu_message = '';
+var menu_input_msg = '';
+var menu_timing = -1;
+var menu_input = false;
+var selected_index = 0;
 function drag(speed) {
 	return 12 * 0.6 * 1.202 * speed ** 2;
 }
--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 function physics() {
-	var thrust = 2500;
+	var thrust = 3500;
 	var thrust_force = thrust * player.engine.current_n1;
-	var drag_force = drag(((player.velocity[0] ** 2) + (player.velocity[1] ** 2) ** 0.5) * 24);
-	if (player.engine.braking) thrust_force -= 1250;
+	var drag_force = drag(
+		(player.velocity[0] ** 2 + player.velocity[1] ** 2) ** 0.5 * 24,
+	);
+	if (player.engine.parking_brake && player.engine.current_n1 > 0.3) player.engine.parking_brake = false;
+	if (player.engine.braking) thrust_force -= 10000;
+	if (player.engine.parking_brake) thrust_force -= 100000;
 	if (player.engine.autothrottle) {
 		var thrust_required = drag_force / thrust;
-		if (thrust_required < player.engine.idle_n1) player.engine.autothrottle = false;
-		if (thrust_required > player.engine.max_n1) thrust_required = player.engine.max_n1;
-		var cmd_throttle = (thrust_required - player.engine.idle_n1) / (player.engine.max_n1 - player.engine.idle_n1);
-		player.engine.cmd_throttle = cmd_throttle;
+		if (thrust_required < player.engine.idle_n1)
+			player.engine.autothrottle = false;
+		else {
+			if (thrust_required > player.engine.max_n1)
+				thrust_required = player.engine.max_n1;
+			var cmd_throttle =
+				(thrust_required - player.engine.idle_n1) /
+				(player.engine.max_n1 - player.engine.idle_n1);
+			player.engine.cmd_throttle = cmd_throttle;
+		}
 	}
 	thrust_force -= drag_force;
-	if (thrust_force < 0) thrust_force = 0;
 	var acceleration = thrust_force / player.mass;
-	var total_velocity = (player.velocity[0] ** 2 + player.velocity[1] ** 2) ** 0.5;
-	if (player.turnRate) player.heading += player.turnRate * total_velocity / 2;
+	var total_velocity =
+		(player.velocity[0] ** 2 + player.velocity[1] ** 2) ** 0.5;
+	if (player.turnRate) {
+		if (player.turnRate > player.maxTurnRate)
+			player.turnRate = player.maxTurnRate;
+		if (player.turnRate < -player.maxTurnRate)
+			player.turnRate = -player.maxTurnRate;
+		player.heading += (player.turnRate * total_velocity) / 2;
+	}
 	while (player.heading < 0) player.heading += 360;
 	while (player.heading >= 360) player.heading -= 360;
 	total_velocity += acceleration / 24;
-	var x = Math.sin(player.heading * Math.PI / 180) * total_velocity;
-	var y = -Math.cos(player.heading * Math.PI / 180) * total_velocity;
+	if (total_velocity < 0) total_velocity = 0;
+	var x = Math.sin((player.heading * Math.PI) / 180) * total_velocity;
+	var y = -Math.cos((player.heading * Math.PI) / 180) * total_velocity;
 	player.velocity[0] = x;
 	player.velocity[1] = y;
 }
 function drawParameter(draw, param) {
-	draw.textAlign('right', 'center');
+	draw.textAlign("right", "center");
 	draw.text(param.name, -30, 0);
 	draw.rect(-7, -1, 14, 2);
 	var param_value = param.value;
@@ -37,64 +54,70 @@ function drawParameter(draw, param) {
 		draw.strokeWeight(1);
 		draw.arc(60, 0, 60, 60, 150, 30);
 		draw.translate(60, 0);
-		draw.fill('green');
+		draw.fill("green");
 		draw.strokeWeight(0);
 		draw.rotate(240);
-		var trend = param.hasOwnProperty('trend');
-		var rotate_amount = (param_value - param.min) / (param.max - param.min) * 240;
+		var trend = param.hasOwnProperty("trend");
+		var rotate_amount =
+			((param_value - param.min) / (param.max - param.min)) * 240;
 		if (trend) {
-			var trend_amount = (param.trend) / (param.max - param.min) * 240 * 240 * 12;
+			var trend_amount =
+				(param.trend / (param.max - param.min)) * 240 * 240 * 12;
 		}
 		draw.rotate(rotate_amount);
-		if (trend && trend_amount > 0) draw.arc(0, 0, 60, 60, 270, 270 + trend_amount);
-		if (trend && trend_amount < 0) draw.arc(0, 0, 60, 60, 270 + trend_amount, 270);
-		draw.fill('lime');
+		if (trend && trend_amount > 0)
+			draw.arc(0, 0, 60, 60, 270, 270 + trend_amount);
+		if (trend && trend_amount < 0)
+			draw.arc(0, 0, 60, 60, 270 + trend_amount, 270);
+		draw.fill("lime");
 		draw.circle(0, 0, 5);
-		draw.rect(-1, -30, 2, 30)
+		draw.rect(-1, -30, 2, 30);
 		draw.rotate(-rotate_amount);
-		draw.fill('white');
+		draw.fill("white");
 		for (var i = 0; i < 11; i++) {
 			draw.rect(-0.5, -35, 1, 5);
 			draw.rotate(24);
 		}
 		draw.rotate(-264);
-		if (param.hasOwnProperty('commanded_value')) {
+		if (param.hasOwnProperty("commanded_value")) {
 			var cmd_value = param.commanded_value;
-			var cmd_rotate_amount = (cmd_value - param.min) / (param.max - param.min) * 240;
-			draw.fill('cyan');
+			var cmd_rotate_amount =
+				((cmd_value - param.min) / (param.max - param.min)) * 240;
+			draw.fill("cyan");
 			draw.rotate(cmd_rotate_amount);
 			draw.triangle(0, -33, -3, -41, 3, -41);
 		}
 		draw.pop();
-		draw.fill('lime');
-		draw.textAlign('center', 'center');
+		draw.fill("lime");
+		draw.textAlign("center", "center");
 		draw.text(value, 60, 20);
-		draw.fill('white');
+		draw.fill("white");
 	} else {
-		draw.textAlign('left', 'center');
+		draw.textAlign("left", "center");
 		draw.text(value, 30, 0);
 	}
 }
 function loadZone(name) {
 	waypoints[name] = [];
-	var numbers = name.split(',');
+	var numbers = name.split(",");
 	var x = Number(numbers[0]);
 	var y = Number(numbers[1]);
 	if (x < -10 || x > 10) return;
 	if (y < -10 || y > 10) return;
-	if (localStorage.getItem('points-' + name)) {
-		waypoints[name] = JSON.parse(localStorage.getItem('points-' + name));
+	if (localStorage.getItem("points-" + name)) {
+		waypoints[name] = JSON.parse(localStorage.getItem("points-" + name));
 		return;
 	}
-	fetch('points/' + name + '.json').then((s) => s.json())
-	.then((s) => {
-		waypoints[name] = s;
-		localStorage.setItem('points-' + name, JSON.stringify(s));
-	});
+	fetch("points/" + name + ".json")
+		.then((s) => s.json())
+		.then((s) => {
+			waypoints[name] = s;
+			localStorage.setItem("points-" + name, JSON.stringify(s));
+		});
 }
 function drawWaypoints(draw, width, height) {
-	var ratio = height * 0.85 / player.map_range;
-	draw.fill('lime');
+	var ratio = (height * 0.85) / player.map_range;
+	draw.fill("lime");
 	draw.strokeWeight(0);
 	var position = [player.x, player.y];
 	var range = player.map_range;
@@ -104,49 +127,83 @@ function drawWaypoints(draw, width, height) {
 	for (var x = min_range[0]; x <= max_range[0]; x++) {
 		for (var y = min_range[1]; y <= max_range[1]; y++) {
 			if (player.map_texture) {
-				if (!textures[player.map_texture]) textures[player.map_texture] = {};
+				if (!textures[player.map_texture])
+					textures[player.map_texture] = {};
 				var texture_object = textures[player.map_texture];
 				if (!texture_object[`${x},${y}`]) {
 					var url = `textures/${player.map_texture}/${x},${y}.png`;
 					texture_object[`${x},${y}`] = globalDraw.loadImage(url);
 				}
 				target = texture_object[`${x},${y}`];
-				if (target) draw.image(texture_object[`${x},${y}`], ((x - player.zoneX) * 500 - player.zoneXOffset) * ratio, ((y - player.zoneY) * 500 - player.zoneYOffset) * ratio, 500 * ratio, 500 * ratio);
+				if (target)
+					draw.image(
+						texture_object[`${x},${y}`],
+						((x - player.zoneX) * 500 - player.zoneXOffset) * ratio,
+						((y - player.zoneY) * 500 - player.zoneYOffset) * ratio,
+						500 * ratio,
+						500 * ratio,
+					);
 			}
-			if (!waypoints[x + ',' + y]) {
-				loadZone(x + ',' + y);
+			if (!waypoints[x + "," + y]) {
+				loadZone(x + "," + y);
 			} else {
-				for (const wpt of waypoints[x + ',' + y]) {
-					var distance = ((player.x - wpt.x) ** 2 + (player.y - wpt.y) ** 2) ** 0.5;
+				for (const wpt of waypoints[x + "," + y]) {
+					var distance =
+						((player.x - wpt.x) ** 2 + (player.y - wpt.y) ** 2) **
+						0.5;
 					if (distance > player.map_range) continue;
-					var angle = Math.atan2(player.y - wpt.y, wpt.x - player.x) * 180 / Math.PI;
+					var angle =
+						(Math.atan2(player.y - wpt.y, wpt.x - player.x) * 180) /
+						Math.PI;
 					angle = player.heading + angle;
-					var angle_rad = angle * Math.PI / 180;
+					var angle_rad = (angle * Math.PI) / 180;
 					var dx = distance * Math.cos(angle_rad);
 					var dy = -distance * Math.sin(angle_rad);
 					dx *= ratio;
 					dy *= ratio;
-					draw.circle(dx, dy, 3);
+					if (player.route_points.includes(wpt.name)) {
+						draw.fill('yellow');
+						draw.circle(dx, dy, 6);
+					} else {
+						draw.circle(dx, dy, 3);
+					}
 					draw.text(wpt.name.toUpperCase(), dx, dy + 10);
+					draw.fill('lime');
 				}
 			}
 		}
 	}
+	draw.push();
+	draw.stroke('yellow');
+	draw.strokeWeight(3);
+	var previous_position = [0, 0];
+	for (const r of player.route) {
+		var heading = r.heading - player.heading;
+		var distance = r.distance;
+		var calibratedPosition = [
+			 distance * Math.sin(heading * Math.PI / 180) * ratio + previous_position[0],
+			-distance * Math.cos(heading * Math.PI / 180) * ratio + previous_position[1]
+		];
+		draw.line(...previous_position, ...calibratedPosition);
+		previous_position = calibratedPosition;
+	}
+	draw.pop();
 }
 function drawDisplay(draw, width, height) {
-	if (!player.display && (nav_display.height >= (globalDraw.height * 0.6))) {
+	draw.clear();
+	if (!player.display && nav_display.height >= globalDraw.height * 0.6) {
 		nav_display.resizeCanvas(width, globalDraw.height * 0.6);
 		display2_header.resizeCanvas(width, globalDraw.height * 0.6);
 	}
-	if (!player.display && (engine_display.height >= globalDraw.height * 0.4)) {
+	if (!player.display && engine_display.height >= globalDraw.height * 0.4) {
 		engine_display.resizeCanvas(width, globalDraw.height * 0.4);
 	}
-	if (player.display == 1 && (nav_display.height < globalDraw.height)) {
-		console.log('resizing to full');
+	if (player.display == 1 && nav_display.height < globalDraw.height) {
+		console.log("resizing to full");
 		nav_display.resizeCanvas(width, globalDraw.height);
 		display2_header.resizeCanvas(width, globalDraw.height);
 	}
-	if (player.display == 2 && (engine_display.height < globalDraw.height)) {
+	if (player.display == 2 && engine_display.height < globalDraw.height) {
 		engine_display.resizeCanvas(width, globalDraw.height);
 	}
 	if (player.display < 2) {
@@ -158,38 +215,72 @@ function drawDisplay(draw, width, height) {
 		if (!player.display) draw.image(engine_display, 0, height * 0.6);
 		else draw.image(engine_display, 0, 0);
 	}
+	if (player.display == 3) {
+		drawMenu(menu_display, width, height);
+		draw.image(menu_display, 0, 0);
+	}
 }
 function update() {
+	if (player.autopilot) {
+		if (!player.route.length) player.autopilot = false;
+		else {
+			var required_heading = player.route[0].heading;
+			player.turnRate = (required_heading - player.heading) / 50;
+		}
+	}
 	physics();
 	player.x += player.velocity[0];
 	player.y += player.velocity[1];
-	if (draw.keyIsDown(37) || draw.keyIsDown(39)) {
-		var amount = 0.05;
-		if (draw.keyIsDown(39)) player.turnRate += amount;
-		if (draw.keyIsDown(37)) player.turnRate -= amount;
-		if (player.turnRate > player.maxTurnRate) player.turnRate = player.maxTurnRate;
-		if (player.turnRate < -player.maxTurnRate) player.turnRate = -player.maxTurnRate;
-	}
-	player.engine.braking = false;
-	if (draw.keyIsDown(38) || draw.keyIsDown(40)) {
-		var amount = 0.01;
-		if (draw.keyIsDown(16)) amount = 0.001;
-		if (draw.keyIsDown(38)) player.engine.cmd_throttle += amount;
-		if (draw.keyIsDown(40)) {
-			player.engine.cmd_throttle -= amount;
-			if (player.engine.current_n1 <= player.engine.idle_n1) player.engine.braking = true;
+	if (player.display != 3) {
+		if (draw.keyIsDown(37) || draw.keyIsDown(39)) {
+			var amount = 0.05;
+			if (draw.keyIsDown(39)) player.turnRate += amount;
+			if (draw.keyIsDown(37)) player.turnRate -= amount;
+			if (player.turnRate < -player.maxTurnRate) player.turnRate = -player.maxTurnRate;
+			if (player.turnRate > player.maxTurnRate) player.turnRate = player.maxTurnRate;
 		}
-		if (player.engine.cmd_throttle > 1) player.engine.cmd_throttle = 1;
-		if (player.engine.cmd_throttle < 0) player.engine.cmd_throttle = 0;
+		player.engine.braking = false;
+		if (draw.keyIsDown(38) || draw.keyIsDown(40)) {
+			var amount = 0.01;
+			if (draw.keyIsDown(16)) amount = 0.001;
+			if (draw.keyIsDown(38)) player.engine.cmd_throttle += amount;
+			if (draw.keyIsDown(40)) {
+				player.engine.cmd_throttle -= amount;
+				if (player.engine.current_n1 <= player.engine.idle_n1)
+					player.engine.braking = true;
+			}
+			if (player.engine.cmd_throttle > 1) player.engine.cmd_throttle = 1;
+			if (player.engine.cmd_throttle < 0) player.engine.cmd_throttle = 0;
+		}
 	}
-	player.engine.current_epr = 0.983 + 0.02 * Math.min(1, player.engine.current_n1 / 0.19) + 0.83 * Math.max(0, (player.engine.current_n1 - 0.19) / 0.85);
-	player.engine.cmd_n1 = player.engine.idle_n1 + player.engine.cmd_throttle * (player.engine.max_n1 - player.engine.idle_n1);
+	player.engine.current_epr =
+		0.983 +
+		0.02 * Math.min(1, player.engine.current_n1 / 0.19) +
+		0.83 * Math.max(0, (player.engine.current_n1 - 0.19) / 0.85);
+	player.engine.cmd_n1 =
+		player.engine.idle_n1 +
+		player.engine.cmd_throttle *
+			(player.engine.max_n1 - player.engine.idle_n1);
 	player.engine.cmd_epr = 1.003 + 0.83 * player.engine.cmd_throttle;
 	player.engine.current_n1 += player.engine.trend_n1;
 	player.zoneX = Math.floor(player.x / 500);
 	player.zoneY = Math.floor(player.y / 500);
 	player.zoneXOffset = player.x - 500 * player.zoneX;
 	player.zoneYOffset = player.y - 500 * player.zoneY;
+	var first_point = player.route[0];
+	if (first_point) {
+		first_point.distance = ((first_point.x - player.x) ** 2 + (first_point.y - player.y) ** 2) ** 0.5;
+		first_point.heading = Math.atan2(first_point.x - player.x, -first_point.y + player.y) * 180 / Math.PI;
+	}
+	for (const r of player.route) {
+		if (r.heading < 0) r.heading += 360;
+	}
+	var first_point = player.route[0];
+	if (first_point && ((first_point.x - player.x) ** 2 + (first_point.y - player.y) ** 2) ** 0.5 < 30) {
+		var name = first_point.name.toLowerCase();
+		if (player.route_points.indexOf(name) > -1) player.route_points.splice(player.route_points.indexOf(name), 1);
+		player.route.splice(0, 1);
+	}
 	draw.clear();
 	drawDisplay(display2, width / 2, height);
 	draw.image(display1, 0, 0);
@@ -198,36 +289,57 @@ function update() {
 		player.engine.current_n1 = player.engine.cmd_n1;
 		player.engine.trend_n1 = 0;
 	}
-	if (Math.abs(player.engine.cmd_n1 - player.engine.current_n1) > 0.005 || Math.abs(player.engine.trend_n1) > 0.001) {
+	if (
+		Math.abs(player.engine.cmd_n1 - player.engine.current_n1) > 0.005 ||
+		Math.abs(player.engine.trend_n1) > 0.001
+	) {
 		var simulated_trend = player.engine.trend_n1;
 		var stopping_distance = 0;
 		while (Math.abs(simulated_trend) > 0.001) {
 			stopping_distance += simulated_trend;
-			if (simulated_trend > player.engine.max_trend_change_n1) simulated_trend -= player.engine.max_trend_change_n1;
-			else if (simulated_trend < -player.engine.max_trend_change_n1) simulated_trend += player.engine.max_trend_change_n1;
+			if (simulated_trend > player.engine.max_trend_change_n1)
+				simulated_trend -= player.engine.max_trend_change_n1;
+			else if (simulated_trend < -player.engine.max_trend_change_n1)
+				simulated_trend += player.engine.max_trend_change_n1;
 			else simulated_trend = 0;
 		}
-		if (Math.abs(stopping_distance * 1.01) > Math.abs(player.engine.cmd_n1 - player.engine.current_n1)) {
-			if (player.engine.trend_n1 < -player.engine.max_trend_change_n1) player.engine.trend_n1 += player.engine.max_trend_change_n1;
-			else if (player.engine.trend_n1 > player.engine.max_trend_change_n1) player.engine.trend_n1 -= player.engine.max_trend_change_n1;
+		if (
+			Math.abs(stopping_distance * 1.01) >
+			Math.abs(player.engine.cmd_n1 - player.engine.current_n1)
+		) {
+			if (player.engine.trend_n1 < -player.engine.max_trend_change_n1)
+				player.engine.trend_n1 += player.engine.max_trend_change_n1;
+			else if (player.engine.trend_n1 > player.engine.max_trend_change_n1)
+				player.engine.trend_n1 -= player.engine.max_trend_change_n1;
 			else player.engine.trend_n1 = 0;
 		} else {
-			if (player.engine.cmd_n1 < player.engine.current_n1) player.engine.trend_n1 -= player.engine.max_trend_change_n1;
+			if (player.engine.cmd_n1 < player.engine.current_n1)
+				player.engine.trend_n1 -= player.engine.max_trend_change_n1;
 			else player.engine.trend_n1 += player.engine.max_trend_change_n1;
-			if (player.engine.trend_n1 > player.engine.max_trend_n1) player.engine.trend_n1 = player.engine.max_trend_n1;
-			if (player.engine.trend_n1 < -player.engine.max_trend_n1) player.engine.trend_n1 = -player.engine.max_trend_n1;
+			if (player.engine.trend_n1 > player.engine.max_trend_n1)
+				player.engine.trend_n1 = player.engine.max_trend_n1;
+			if (player.engine.trend_n1 < -player.engine.max_trend_n1)
+				player.engine.trend_n1 = -player.engine.max_trend_n1;
 		}
 	}
 }
 function keydown(ev) {
-	if (ev.keyCode == 67) player.engine.autothrottle = !player.engine.autothrottle;
+	if (player.display == 3) return;
+	if (ev.keyCode == 80) player.autopilot = !player.autopilot;
+	if (ev.keyCode == 67)
+		player.engine.autothrottle = !player.engine.autothrottle;
 	if (ev.keyCode == 86) {
 		player.display += 1;
-		if (player.display > 2) player.display = 0;
+		if (player.display > 3) player.display = 0;
 	}
+	if (ev.keyCode == 66) player.engine.parking_brake = !player.engine.parking_brake;
 }
-addEventListener('keydown', keydown);
+addEventListener("keydown", keydown);
 var waypoints = {};
+var all_waypoints = {};
+fetch("all_points.json")
+	.then((s) => s.json())
+	.then((s) => (all_waypoints = s));
 var textures = {};
 var waypointsToLoad = [];
 var width = 960;
@@ -239,22 +351,28 @@ var s = function (sketch) {
 		updateInterval = setInterval(update, 1000 / 24);
 		display1 = draw.createGraphics(width / 2, height);
 		display2 = draw.createGraphics(width / 2, height);
-		display2.textFont('consolas');
-		display2.angleMode('degrees');
-		display2.background('black');
-		display2.textAlign('center', 'center');
+		display2.textFont("consolas");
+		display2.angleMode("degrees");
+		display2.background("black");
+		display2.textAlign("center", "center");
 		nav_display = draw.createGraphics(width / 2, height);
-		nav_display.textFont('consolas');
-		nav_display.angleMode('degrees');
-		nav_display.textAlign('center', 'center');
+		nav_display.textFont("consolas");
+		nav_display.angleMode("degrees");
+		nav_display.textAlign("center", "center");
 		engine_display = draw.createGraphics(width / 2, height);
-		engine_display.textFont('consolas');
-		engine_display.angleMode('degrees');
-		engine_display.stroke('white');
-		engine_display.textAlign('center', 'center');
+		engine_display.textFont("consolas");
+		engine_display.angleMode("degrees");
+		engine_display.stroke("white");
+		engine_display.textAlign("center", "center");
 		engine_display.textSize(15);
 		display2_header = draw.createGraphics(width / 2, height);
-	}
+		menu_display = draw.createGraphics(width / 2, height);
+		menu_display.textFont("consolas");
+		menu_display.angleMode("degrees");
+		menu_display.stroke("white");
+		menu_display.textAlign("center", "center");
+		menu_display.textSize(15);
+	};
 };
-var draw = new p5(s, 'pad');
+var draw = new p5(s, "pad");
 var globalDraw = draw;
